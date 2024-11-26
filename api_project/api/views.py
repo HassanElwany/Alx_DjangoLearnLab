3c937897c4a4a37c51b4a5765b5cc4613f1d1c57
HassanElwany <elwany_hassan@hotmail.com>
2024-11-26 20:24:01 +0300

    2. Implementing CRUD Operations with ViewSets and Routers in Django REST Framework 0001

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,7 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets 
 from .models import Book
 from .serializers import BookSerializer
 
-class BookViewSet(ModelViewSet):
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
