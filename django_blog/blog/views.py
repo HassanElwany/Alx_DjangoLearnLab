9af2eb2f5575d50a1692e859d29db88d622015c4
HassanElwany <elwany_hassan@hotmail.com>
2024-12-16 16:41:28 +0300

    Creating Blog Post Management Features 0002

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,67 +1,60 @@
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile, Post
 from .forms import ProfileUpdateForm, CustomUserCreationForm
 from django.contrib import messages
 
 
-@method_decorator(login_required, name='dispatch')
-class ProfileListView(ListView):
-    model = Profile
-    template_name = "blog/profile_list.html"
-    context_object_name = "profiles"
-    paginate_by = 10  # Optional: Paginate profiles
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
 
 
-@method_decorator(login_required, name='dispatch')
-class ProfileDetailView(DetailView):
-    model = Profile
-    template_name = "blog/profile_detail.html"
-    context_object_name = "profile"
-
-    def get_object(self):
-        return self.request.user.profile  # Ensure the user views only their profile
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+    context_object_name = "post"
 
 
-@method_decorator(login_required, name='dispatch')
-class ProfileUpdateView(UpdateView):
-    model = Profile
-    form_class = ProfileUpdateForm
-    template_name = "blog/profile_form.html"
-    success_url = reverse_lazy("profile_detail")
-
-    def get_object(self):
-        return self.request.user.profile  # Restrict updates to the logged-in user's profile
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = "blog/post_form.html"
+    success_url = reverse_lazy("post_list")
 
     def form_valid(self, form):
-        messages.success(self.request, "Your profile has been updated successfully.")
+        form.instance.author = self.request.user
         return super().form_valid(form)
 
 
-class RegisterView(CreateView):
-    form_class = CustomUserCreationForm
-    template_name = "blog/register.html"
-    success_url = reverse_lazy("login")
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = "blog/post_form.html"
+    success_url = reverse_lazy("post_list")
 
-    def form_valid(self, form):
-        response = super().form_valid(form)
-        # Create profile for new user
-        Profile.objects.create(user=self.object)
-        messages.success(self.request, "Registration successful! Please log in.")
-        return response
+    def test_func(self):
+        """Ensure only the author can update the post."""
+        post = self.get_object()
+        return post.author == self.request.user
+
+    def handle_no_permission(self):
+        messages.error(self.request, "You are not authorized to edit this post.")
+        return super().handle_no_permission()
 
 
-class ProfileDeleteView(DeleteView):
-    model = User
-    template_name = "blog/profile_confirm_delete.html"
-    success_url = reverse_lazy("register")  # Redirect to registration after deletion
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = "blog/post_confirm_delete.html"
+    success_url = reverse_lazy("post_list")
 
-    def get_object(self):
-        return self.request.user  # Allow users to delete their own account
+    def test_func(self):
+        """Ensure only the author can delete the post."""
+        post = self.get_object()
+        return post.author == self.request.user
 
-    def delete(self, request, *args, **kwargs):
-        messages.success(request, "Your account has been deleted.")
-        return super().delete(request, *args, **kwargs)
+    def handle_no_permission(self):
+        messages.error(self.request, "You are not authorized to delete this post.")
+        return super().handle_no_permission()
