7179e1d9eb2494229ea062cf974f1f8e45d5e914
HassanElwany <elwany_hassan@hotmail.com>
2024-12-19 10:55:34 +0300

    Project Setup and User Authentication for a Social Media API 0000

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,24 +1,25 @@
 from rest_framework import serializers
-from .models import CustomUser
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
+# Get the custom user model
+User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CustomUser
+        model = User
         fields = ['username', 'password', 'email']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(
+        # Create a new user
+        user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password']
         )
+        # Create an authentication token for the new user
+        Token.objects.create(user=user)
         return user
 
 class LoginSerializer(serializers.Serializer):
@@ -26,7 +27,8 @@ class LoginSerializer(serializers.Serializer):
     password = serializers.CharField(write_only=True)
 
     def validate(self, data):
-        user = CustomUser.objects.filter(username=data['username']).first()
+        # Authenticate the user
+        user = User.objects.filter(username=data['username']).first()
         if user and user.check_password(data['password']):
             return user
         raise serializers.ValidationError("Invalid credentials")
