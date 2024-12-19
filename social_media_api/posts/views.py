ae26b4aa35789315959452bf00e3c190e148c593
HassanElwany <elwany_hassan@hotmail.com>
2024-12-19 16:45:48 +0300

    Implementing Posts and Comments Functionality 000013

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,14 +1,38 @@
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import permissions
-from .models import Post  # Assuming a Post model exists in posts app
-from .serializers import PostSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
+from .models import Post, Comment  
+from .serializers import PostSerializer, CommentSerializer
 
 class FeedView(APIView):
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     def get(self, request):
         following_users = request.user.following.all()
         posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
+
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def get_queryset(self):
+        return self.queryset.filter(author=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def get_queryset(self):
+        return self.queryset.filter(author=self.request.user)
